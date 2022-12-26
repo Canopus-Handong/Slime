8b7f4f863d53bbe84c34723bbc5dbd3440fe0123
inbaea <49175916+inbaea@users.noreply.github.com>
2022-12-26 15:57:56 +0900

    update enemy Moving

--- Assets/Scipts/Character/Enemy/EnemyMove2.cs
+++ Assets/Scipts/Character/Enemy/EnemyMove2.cs
@@ -9,16 +9,20 @@ public class EnemyMove2 : MonoBehaviour
     public GameObject Platformscript;
     Rigidbody2D rigid;
     public Collider2D detectzone;
-    public int enemyMove = 0;
+    public int enemyMove;
     SpriteRenderer spriteRenderer;
 
     public bool follow = false;
+    public bool notfollowButmove = false;
+
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         Physics2D.IgnoreCollision(Enemyscript.GetComponent<Collider2D>(), Platformscript.GetComponent<Collider2D>(), true);
+
+        Invoke("Moving", 3);
     }
 
     void Update()
@@ -28,15 +32,27 @@ public class EnemyMove2 : MonoBehaviour
 
     void FixedUpdate()
     {
-        SelectMoving();
-
+        if (follow)
+            SelectMoving();
+        if (Enemyscript.GetComponent<Enemy>().health <= 0 && notfollowButmove)
+        {
+            enemyMove = 0;
+            CancelInvoke();
+        }
         //Check Platform
         Vector2 frontVec = new Vector2(rigid.position.x + enemyMove * 0.5f, rigid.position.y);
         RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
 
         //If there is no platform, stop
         if (rayHit.collider == null) {
-            enemyMove = 0;
+            if (!notfollowButmove)
+                enemyMove = 0;
+            if (notfollowButmove)
+            {
+                enemyMove = enemyMove * -1;
+                CancelInvoke();
+                Invoke("Moving", 5);
+            }
         }
 
         //If monster turn back, turn image
@@ -65,9 +81,26 @@ public class EnemyMove2 : MonoBehaviour
         }
     }
 
+        void Moving()
+    {
+        notfollowButmove = true;
+
+        if (follow)
+            return;
+        if (Enemyscript.GetComponent<Enemy>().health <= 0){
+            enemyMove = 0;
+            return;
+        }
+        else {
+            enemyMove = Random.Range(-1,2);
+        }
+
+        Invoke("Moving", 5);
+    }
+
     void FollwTarget()
     {
-        if(follow)
+        if(follow || notfollowButmove)
         {
             //Moving
             rigid.velocity = new Vector2(enemyMove, rigid.velocity.y);
@@ -80,12 +113,21 @@ public class EnemyMove2 : MonoBehaviour
     private void OnTriggerStay2D(Collider2D detect)
     {
         if (detect.tag == "Player")
+        {
             follow = true;
+            notfollowButmove = false;
+            CancelInvoke();
+        }
+            
     }
 
     private void OnTriggerExit2D(Collider2D detect)
     {
         if (detect.tag == "Player")
+        {
             follow = false;
+            Invoke("Moving", 5);
+        }
+            
     }
 }
