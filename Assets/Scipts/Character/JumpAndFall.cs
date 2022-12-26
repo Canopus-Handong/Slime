3f7f3bd8b5d62ee7323ed031530cf77f5a6f5189
inbaea <49175916+inbaea@users.noreply.github.com>
2022-12-26 09:47:25 +0900

    change test scene

--- Assets/Scipts/Character/JumpAndFall.cs
+++ Assets/Scipts/Character/JumpAndFall.cs
@@ -7,7 +7,7 @@ public class JumpAndFall : MonoBehaviour
     Rigidbody2D rigid;
     Collider2D playerCollider, platformCollider;
     public GameObject Platformscript;
-    public GameObject Controllscript;
+    public GameObject Slimescript;
     public bool fallGround;
     float moveX;
 
@@ -23,12 +23,12 @@ public class JumpAndFall : MonoBehaviour
 
     IEnumerator LayerOpenClose()
     {
-        Controllscript.GetComponent<PlayerController>().canJump = false;
+        Slimescript.GetComponent<PlayerController>().canJump = false;
         fallGround = true;
         IgnoreTure();
         yield return new WaitForSeconds(0.2f);
         IgnoreFalse();
-        Controllscript.GetComponent<PlayerController>().canJump = true;
+        Slimescript.GetComponent<PlayerController>().canJump = true;
         fallGround = false;
     }
 
