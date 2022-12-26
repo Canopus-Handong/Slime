77d5537a4bbb1669e7bfd313bdd5feb06bc81cac
inbaea <49175916+inbaea@users.noreply.github.com>
2022-12-26 13:53:41 +0900

    enemy moving

--- Assets/Scipts/Character/Enemy/EnemyMove2.cs
+++ Assets/Scipts/Character/Enemy/EnemyMove2.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class EnemyMove2 : MonoBehaviour
 {
+    public GameObject Enemyscript;
+    public GameObject Detectzonescript;
+    public GameObject Slimescript;
+    public GameObject Platformscript;
     Rigidbody2D rigid;
-    public Transform target;
+    public Collider2D detectzone;
+    public int enemyMove = 0;
+    SpriteRenderer spriteRenderer;
 
-    [Header("추격 속도")]
-    [SerializeField] [Range(1f, 4f)] float moveSpeed = 3f;
-
-    [Header("근접 거리")]
-    [SerializeField] [Range(1f, 4f)] float moveDistance = 1f;
-
-    bool follow = false;
+    public bool follow = false;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        detectzone = Detectzonescript.GetComponent<BoxCollider2D>();
+        Physics2D.IgnoreCollision(Enemyscript.GetComponent<Collider2D>(), Platformscript.GetComponent<Collider2D>(), true);
     }
 
     void Update()
@@ -25,21 +28,65 @@ public class EnemyMove2 : MonoBehaviour
         FollwTarget();
     }
 
+    void FixedUpdate()
+    {
+        SelectMoving();
+
+        //Check Platform
+        Vector2 frontVec = new Vector2(rigid.position.x + enemyMove * 0.5f, rigid.position.y);
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+        //If there is no platform, stop
+        if (rayHit.collider == null) {
+            enemyMove = 0;
+        }
+
+        //If monster turn back, turn image
+        if (enemyMove != 0){
+            spriteRenderer.flipX = enemyMove == 1;
+        }
+    }
+
+    void SelectMoving()
+    {
+        //If no HP, stop
+        if (Enemyscript.GetComponent<Enemy>().health <= 0)
+        {
+            enemyMove = 0;
+            return;
+        }
+        else if ((Slimescript.GetComponent<Transform>().position.x - Enemyscript.GetComponent<Transform>().position.x) < 0)
+        {
+            enemyMove = -1;
+            return;
+        }
+        else if ((Slimescript.GetComponent<Transform>().position.x - Enemyscript.GetComponent<Transform>().position.x) > 0)
+        {
+            enemyMove = 1;
+            return;
+        }
+    }
+
     void FollwTarget()
     {
-        if(Vector2.Distance(transform.position, target.position) > moveDistance && follow)
-            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        if(follow)
+        {
+            rigid.velocity = new Vector2(enemyMove, rigid.velocity.y);
+        }
+        
         else
             rigid.velocity = Vector2.zero;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D detect)
     {
-        follow = true;
+        if (detect.tag == "Player")
+            follow = true;
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D detect)
     {
-        follow = false;
+        if (detect.tag == "Player")
+            follow = false;
     }
 }
