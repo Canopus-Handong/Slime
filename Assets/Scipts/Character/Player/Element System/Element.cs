1b1bc74eb712f412fdb14cbebfe35369b6a5fade
joon0017 <97450065+joon0017@users.noreply.github.com>
2022-12-27 09:47:43 +0900

    organize scripts

---
+++ Assets/Scipts/Character/Player/Element System/Element.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementException : Exception
+{
+    public ElementException(){}
+    public ElementException(string message) : base(message){}
+    public ElementException(string message, Exception inner) : base(message, inner){}
+}
+public abstract class Element : MonoBehaviour
+{
+    private GameObject GM;
+    // Start is called before the first frame update
+    void Awake(){
+        GM = this.gameObject.GetComponent<GameManager>();
+    }
+
+    public abstract void useElement();
+    public abstract void useElementTrace();
+}
